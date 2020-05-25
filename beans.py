44f3840a6a43e29ad279e93c32c6fb8984707529
toshitaka18 <toshitaka0018@gmail.com>
2020-05-25 17:04:29 +0900

    100粒目200粒目判別式を実装（仮）

--- beans.py
+++ beans.py
@@ -17,8 +17,30 @@ class Beans():
                 total_beans += age
             if total_beans < 100:
                 print('100粒目の豆を食べたひとはいません。')
+            else:
+                count_beans = 0
+                i = 0
+                for name1, age1 in zip(names, ages):
+                    count_beans += age1
+                    if count_beans >= 100:
+                        print('100粒目の豆を食べたひとは、{0}です。 '.format(name1))
+                        break
+                    else:
+                        i += 1
+                        continue
 
-            print('合計は{0}個です'.format(total_beans))
+                count_beans1 = 0
+                j = 0
+                for name2, age2 in zip(names, ages):
+                    count_beans1 += age2
+                    if count_beans1 >= 200:
+                        print('200粒目の豆を食べたひとは、{0}です。 '.format(name2))
+                        break
+                    else:
+                        j += 1
+                        continue
+
+            print('家族全員で食べた豆の数は{0}個です'.format(total_beans))
             print(Thanks_message.statment)
         elif app2_string == 'n':
             print(Exit_message.statment)

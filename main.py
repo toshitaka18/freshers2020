2c79417c72f8502cce4e31c5239decfe17b118ac
toshitaka18 <toshitaka0018@gmail.com>
2020-05-25 14:27:00 +0900

    運勢プログラムのインスタンス生成

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from message import Error_message, Thanks_message, Exit_message, Name_error_message
 from calc_number import Calc_sort
 from beans import Beans
-import random
+from fortune import Fortune
 
 try:
     app_number = int(input('＊＊＊ プログラミング体験アプリへ ようこそ！＊＊＊ \n'
@@ -19,7 +19,7 @@ try:
         app1_string = input('昇順に並べ替えますか？ (y/n) ==>')
         app1_amount = int(input('並べ替えしたい数字の数はいくつですか？(2-5)==> '))
 
-        app1 = Calc_sort()  # インスタンスの生成しxに代入
+        app1 = Calc_sort()  # インスタンスの生成し変数に代入
         result_1 = app1.sort_number(app1_string, app1_amount)  # インスタンスメソッドによって得た戻り値をresult_1に代入
         if len(result_1) == 2:  # 戻り値の中身が二つあれば、それぞれ表示。そうでなければ1つだけ表示
             print(result_1[0])
@@ -42,27 +42,16 @@ try:
               '＊＊＊＊＊＊＊＊＊＊＊＊＊')
 
         count_name = 0
-        while count_name < 3:
+        while count_name < 3:  # while文を最大三回回す。
             user_name = input('あなたの名前を入力してください。==> ')
-            if user_name:
+            if user_name:  # 三回以内に名前が入力されたら（user_nameが存在したら）while文を抜ける。
                 break
             else:
-                count_name += 1
-        if user_name:
-            lucky_number = random.randint(1, 11)
-            if lucky_number == 1:
-                print('{0}さんの運勢は、大吉です。おめでとうございます！'.format(user_name))
-            elif lucky_number == 2 or lucky_number == 3:
-                print('{0}さんの運勢は、吉です。'.format(user_name))
-            elif lucky_number == 4 or lucky_number == 5:
-                print('{0}さんの運勢は、小吉です。'.format(user_name))
-            elif lucky_number == 6:
-                print('{0}さんの運勢は、凶です。ファイト！'.format(user_name))
-            else:
-                print('{0}さんの運勢は、中吉です。'.format(user_name))
-            print(Thanks_message.statment)
-        else:
-            print(Name_error_message.statment)
+                count_name += 1  # 名前が入力されない場合はカウントを１増やす
+
+        app3 = Fortune()  # インスタンスの生成し変数に代入
+        result_3 = app3.lucky(user_name)  # インスタンスメソッドによって得た戻り値をresult_3に代入
+
     elif app_number == 4:
         print('電気自転車充電プログラム')
     elif app_number == 5:

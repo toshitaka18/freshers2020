a8fef1909cc664b094020e4b6f2ac7c41e930aa4
toshitaka18 <toshitaka0018@gmail.com>
2020-05-28 14:04:39 +0900

    数字並び替えプログラム（100を超えた入力を拒否）

--- calc_number.py
+++ calc_number.py
@@ -5,7 +5,12 @@ class Calc_sort:
         if app1_amount >= 2 and app1_amount <= 5:  # 数字の個数が2~5の場合とそれ以外で場合分け
             app1_list = []
             for i in range(1, app1_amount + 1):
-                app1_list.append(int(input(str(i) + "つ目")))  # app1_listに対して、appendで要素を一つずつ追加
+                sort_number = int(input(str(i) + "つ目"))
+                if sort_number <= 100:
+                    app1_list.append(sort_number)  # app1_listに対して、appendで要素を一つずつ追加
+                else:
+                    return Error_message.statment
+
             if app1_string == 'y':  # 昇順か降順かで場合わけ
                 print('昇順に並び替えます')
                 app1_list.sort()  # sortで昇順に並び替える。返される値はNone、なので結果を表示するにはprint(list名)
